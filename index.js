04cd2efed932c199da6a8b615868f6e6af5a869f
qAbduallajon <azikmelor7705@gmail.com>
2025-01-27 16:46:15 +0500

    Routing

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.get("/", (rea, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
 
 const PORT = process.env.PORT || 4100;
 app.listen(PORT, () => console.log("Server is runing on port:", PORT));
