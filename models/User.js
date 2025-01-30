9af33aed12ac0e7eb10984c6bbe102b0a52091f4
qAbduallajon <azikmelor7705@gmail.com>
2025-01-30 23:40:43 +0500

    Models mongoose, User

---
+++ models/User.js
@@ -0,0 +1,11 @@
+import { model, Schema } from "mongoose";
+
+const AuthSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = model("User", AuthSchema);
+export default User;
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import User from "../models/User.js";
 const router = Router();
 
 router.get("/login", (req, res) => {
@@ -15,13 +16,19 @@ router.get("/register", (req, res) => {
   });
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   console.log(req.body);
   res.redirect("/");
 });
 
-router.post("/register", (req, res) => {
-  console.log(req.body);
+router.post("/register", async (req, res) => {
+  const userData = {
+    firstName: req.body.firstname,
+    lastName: req.body.lastname,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  await User.create(userData);
   res.redirect("/login");
 });
 
